384785096e07a70a5835706aed3557eb48da6aec
MAXBIT\v.dolgiy <v.dolgiy@jetmail.cc>
2026-04-06 14:10:37 +0300

    feat: add new function in app

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,24 @@
 <script setup>
 import PlayBar from '@/components/play-bar/index.vue'
-import musicList from '@/components/music-list/index.vue'
+import musicList from '@/components/music-list/music-item/index.vue'
+import { music } from "@/store/pinia/music/index.js";
+import { onMounted, watch } from "vue";
+import { storeToRefs } from "pinia"
 
-import { player } from '@/store/pinia/player/index.ts'
-import { music } from '@/store/pinia/music/index.ts'
+const musicStore = music()
+
+const { getMusicList } = storeToRefs(music())
+
+onMounted(() => {
+  musicStore.fetchDataMusic()
+})
 </script>
 
 <template>
   <div class="main-wr">
-    <musicList />
-    <PlayBar />
+    <musicList :musicList="getMusicList" />
+    <PlayBar :musicList="getMusicList" />
   </div>
-
-  {{ music().getMusicList }}
 </template>
 
 <style src="./assets/scss/main.scss" lang="scss" />
