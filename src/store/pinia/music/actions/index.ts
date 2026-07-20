0055eb179f8e385a1ee3b947be87b0cc12e48401
v.dolgiy <vadim.dolgiy.2003@mail.ru>
2026-07-20 19:02:03 +0300

    refactor: change getters get music on get music & get audio

--- src/store/pinia/music/actions/index.ts
+++ src/store/pinia/music/actions/index.ts
@@ -3,12 +3,11 @@ import mockMusic from '../mockMusic.json'
 const actions = {
     fetchDataMusic() {
         this.musicList = mockMusic.map((musicItem: Object) => { return musicItem })
+        console.log(this.musicList)
     },
     setActiveMusic(id: number, url: string) {
         this.activeMusic = this.musicList[id]
         this.audio.src = url
-
-        console.log(this.activeMusic)
     },
     setRepeatMusic() {
         this.onEnded = !this.onEnded
